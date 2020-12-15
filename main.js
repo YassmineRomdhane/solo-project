ef24b749aa9e10be9b610fc613f823843838f6f3
YassmineRomdhane <yassmineromdhane1996@gmail.com>
2020-12-15 18:27:42 +0100

    init

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
-$(".form").hide()
-$(".imgf").hide()
-$('#data').hide()
-var arr = [ "https://cdn.shopify.com/s/files/1/2048/8041/products/Flower_Drift_Floret-7_700x700.jpg?v=1569518617" , "https://cdn.shopify.com/s/files/1/2048/8041/products/Flower_Drift_Floret-7_700x700.jpg?v=1569518617" ,"https://cdn.shopify.com/s/files/1/2048/8041/products/Flower_Drift_Floret-7_700x700.jpg?v=1569518617" ,"https://cdn.shopify.com/s/files/1/2048/8041/products/Flower_Drift_Floret-7_700x700.jpg?v=1569518617" ,"https://cdn.shopify.com/s/files/1/2048/8041/products/Flower_Drift_Floret-7_700x700.jpg?v=1569518617" ,"https://cdn.shopify.com/s/files/1/2048/8041/products/Flower_Drift_Floret-7_700x700.jpg?v=1569518617" ,"https://cdn.shopify.com/s/files/1/2048/8041/products/Flower_Drift_Floret-7_700x700.jpg?v=1569518617" ,];
-$("body").append("<ul></ul>");
-for (var i = 0; i < arr.length; i++) {
-  $(".types").append('<img id="image" src=' + arr[i] + '/>' );
-} 
 
-// var flowers = ["https://i.pinimg.com/originals/35/63/cc/3563ccf3fb0ee7de471f33394b6e6d95.jpg", "https://5.imimg.com/data5/ZR/OO/EA/SELLER-34246236/tulip-flower-500x500.jpg", "https://cdn.shopify.com/s/files/1/2048/8041/products/Flower_Drift_Floret-7_700x700.jpg?v=1569518617","https://5.imimg.com/data5/WH/MA/MY-3674107/orchids-flowers-500x500.jpg","https://images-na.ssl-images-amazon.com/images/I/610ykpFyqSL._SL1000_.jpg","https://www.gardenia.net/storage/app/public/guides/detail/eLtzXyWRxGUO34KrdI1KC2VLSYdQ3AYKA2Cvj3ni.jpeg"];
-// $("body").append("<ul></ul>");
-// for (var i = 0; i < arr.length; i++) {
-//   $(".types").append('<img src="https://i.pinimg.com/originals/35/63/cc/3563ccf3fb0ee7de471f33394b6e6d95.jpg" >');
-// } 
-// $('.input1').click(function(){
-//    $('.types').hide('fast')
-//    $(".form").show('slow')
-// })
-// $('.myflowers').click(function(){
-//   $('.imgf').toggle()
-// })
+$('#data').hide()
+$('.store').hide()
+$('.in').hide()
+$('#click').click(function(){
+   $('.store').toggle('slow')
+})
+
+$('.input1').click(function(){
+   $('.store').toggle('slow')
+   $('#p1').hide('fast')
+   $('h1').hide('fast')
+   $('.in').show('slow')
+   console.log($('.inputOfbox').val())
+})
+
 var data="" 
 
+// //   var arr =["firstName : " + $('#fname').val()," lastName : " +  $('#lname').val()]
+// //   $("body").append("<ul></ul>");
+// //   for (var i = 0; i < arr.length; i++) {
+// //    $("ul").append("<li>" + arr[i] + "</li>");
+// //  }
+// //    console.log(arr)
+
 $('.input2').click(function(){
-//      document.getElementById("data").innerHTML = "firstName : " + $('#fname').val() + " " +" lastName : " + " " + $('#lname').val() + " " + "Address"+ $("#address").val()
-//  $('#data').show()
-data= $('#fname').val() + " " + $('#lname').val() + " " + $("#address").val()
+     document.getElementById("data").textContent = "firstName : " + $('#fname').val() + "\n " +" lastName : " + "\n " + $('#lname').val() + " \n" + "Address :"+ $("#address").val() + "\n " + "e-mail address :" + $("#address").val()
+ $('#data').show()
+data= $('#fname').val() + " " + $('#lname').val() + " " + $("#address").val() +  " " + $("#add").val()
  localStorage.setItem($('#fname').val(), data);
 })
 
+localStorage.setItem($('#fname').val , data)
+// // // var h=  localStorage.setItem("")
 
-//   data= $('#fname').val + " " + $('#lname').val + " " + $("#address").val
-  
-// document.getElementById("data").innerHTML = "firstName" + $('#fname').val + " " +"lastName" + " " + $('#lname').val + " " + "Address"+ $("#address").val
+// // var arr = [1, 2, 3, 4];
 
+// // ​
+// // for (var i = 0; i < arr.length; i++) {
+// //   $("ul").append("<li>" + arr[i] + "</li>");
+// }
 
-// localStorage.setItem($('#fname').val , data)
-// var h=  localStorage.setItem("")
+ 
